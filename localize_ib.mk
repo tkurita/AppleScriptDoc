5fe12bddbc9ccf09919665fa3b35ac8061a7fadf
Tetsuro Kurita <tkurita@mac.com>
2016-02-11 17:33:08 +0900

    Update settings of localization process

--- localize_ib.mk
+++
@@ -1,6 +0,0 @@
-# following variables must be given as command line arguments
-#target_lang = Japanese
-#target_nib = Japanese.lproj/MainMenu.nib
-
-$(target_nib): $(target_lang).lproj/%.nib: English.lproj/%.nib $(target_lang).lproj/%.strings
-	./localize_ib.sh $< $@
